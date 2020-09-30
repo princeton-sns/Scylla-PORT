450fb20b0a11438ec3b097e0bd8e5c6883766cd6
Haonan Lu <haonanl@cs.princeton.edu>
2020-09-29 23:42:33 -0700

    Update get-throughput.bash

--- eval-control/data_proc/get-throughput.bash
+++ eval-control/data_proc/get-throughput.bash
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-source ./parameters.bash
+source ../parameters.bash
 
+# --- change these variables accordingly ---
 file="1523953294"
 system="Scylla-original"
-thread="32"
-workload="workloada"
+# ------------------
 
 clients=$(cat ${client_nodes})
 result=""
 for client in ${clients}; do
     ssh_output=$(ssh ${client} -o StrictHostKeyChecking=no " \
-        cat /local/scylladb/experiments/${file}/${system}/trial-1/${workload}+keysperop_5+threads_${thread}.data \
+        cat ${data_dir}/${file}/${system}/trial-1/${workload}+keysperop_${keysperop}+threads_${threads}.data \
         | grep OVERALL | grep Throughput \
     ")
     echo $ssh_output >> throughput
 done
 thruput=$(cat throughput | awk '{print $3}' | awk -F"." '{print $1}' | awk '{s+=$1} END {print s}')
-echo "$system : $thread : $thruput" 
+echo "$system : $threads : $thruput" 
 rm throughput
