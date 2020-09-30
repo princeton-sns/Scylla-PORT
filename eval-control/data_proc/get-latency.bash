f2cf08c3a86d8bf4dd4a2085754bf975522d1ba7
Haonan Lu <haonanl@cs.princeton.edu>
2020-09-29 23:40:21 -0700

    Update get-latency.bash

--- eval-control/data_proc/get-latency.bash
+++ eval-control/data_proc/get-latency.bash
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-source ./parameters.bash
+source ../parameters.bash
 
+#--- global variables, change these accordingly ---
 file="1523953294"
 system="Scylla-original"
-thread="32"
-workload="workloada"
+num_clients=8
+# -----------------
 
 clients=$(cat ${client_nodes})
 result=""
 for client in ${clients}; do
     ssh_output=$(ssh ${client} -o StrictHostKeyChecking=no " \
-        cat /local/scylladb/experiments/${file}/${system}/trial-1/${workload}+keysperop_5+threads_${thread}.data \
+        cat ${data_dir}/${file}/${system}/trial-1/${workload}+keysperop_${keysperop}+threads_${threads}.data \
         | grep READ | grep AverageLatency \
     ")
     echo $ssh_output >> latency
 done
 all_latency=$(cat latency | awk '{print $3}' | awk -F"." '{print $1}' | awk '{s+=$1} END {print s}')
 #echo $all_latency
-latency=$((all_latency / 8))
-echo "$system : $thread : $latency" 
+latency=$((all_latency / ${num_clients}))
+echo "$system : $threads : $latency" 
 rm latency
